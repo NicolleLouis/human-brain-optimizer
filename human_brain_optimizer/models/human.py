8f8ea8e626e3806ee347c58921e7e0f69ef8cdaf
Louis Nicolle <louisxnicolle@gmail.com>
2025-02-09 00:01:20 +0100

    Create base humans

---
+++ human_brain_optimizer/models/human.py
@@ -0,0 +1,25 @@
+import random
+
+
+class Human:
+    MAXIMUM_INDICATOR_LEVEL = 25
+
+    def __init__(self):
+        self.food_level = self.MAXIMUM_INDICATOR_LEVEL
+        self.energy_level = self.MAXIMUM_INDICATOR_LEVEL
+        self.age = 0
+        self.dead = False
+
+    def turn_consequence(self):
+        self.food_level -= 1
+        self.energy_level -= 1
+        self.death_check()
+
+    def death_check(self):
+        death_probability = 0
+        if self.energy_level <= 0 and self.food_level <= 0:
+            death_probability = 50
+        elif self.energy_level <= 0 or self.food_level <= 0:
+            death_probability = 10
+        if random.randint(0, 100) < death_probability:
+            self.dead = True
---
+++ human_brain_optimizer/tests/models/test_human.py
@@ -0,0 +1,45 @@
+import pytest
+
+from human_brain_optimizer.models.human import Human
+
+
+@pytest.fixture
+def human():
+    return Human()
+
+
+def test_initial_conditions(human):
+    assert human.food_level == Human.MAXIMUM_INDICATOR_LEVEL
+    assert human.energy_level == Human.MAXIMUM_INDICATOR_LEVEL
+    assert human.age == 0
+    assert not human.dead
+
+
+def test_turn_consequence(human):
+    human.turn_consequence()
+    assert human.food_level == Human.MAXIMUM_INDICATOR_LEVEL - 1
+    assert human.energy_level == Human.MAXIMUM_INDICATOR_LEVEL - 1
+
+
+def test_death_probability_both_defection(human):
+    human.food_level = 0
+    human.energy_level = 0
+    death_count = 0
+    for _ in range(1000):
+        human.death_check()
+        if human.dead:
+            death_count += 1
+        human.dead = False
+    assert 600 > death_count > 400
+
+
+def test_death_probability_single_defection(human):
+    human.food_level = 10
+    human.energy_level = 0
+    death_count = 0
+    for _ in range(1000):
+        human.death_check()
+        if human.dead:
+            death_count += 1
+        human.dead = False
+    assert 150 > death_count > 50
